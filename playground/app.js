f6205740e24391c60a8026cfef2a7d5d06d479b9
Ian Moss <github@ilmc.co.uk>
2017-01-05 17:17:22 +0000

    Added swagger ui to playground app - started modifying the swagger.yaml file

--- playground/app.js
+++ playground/app.js
@@ -2,6 +2,10 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+
+// Include the Swagger UI in the app
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+
 module.exports = app; // for testing
 
 var config = {
@@ -11,13 +15,16 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+  // Use Swagger UI in this app
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
+
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
+  var port = process.env.PORT || 3000;
+  app.listen('3000', '0.0.0.0');
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    //console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   }
 });
